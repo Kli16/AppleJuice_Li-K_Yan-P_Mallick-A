e94bb2cf9b1060c05594b09b1a0e8e430387e933
Alitquan <amallick@stuy.edu>
2017-03-12 03:26:49 -0400

    Changed format of data for easier data analysis

--- runTimeTest.java
+++ runTimeTest.java
@@ -57,53 +57,90 @@ public class runTimeTest extends QuickSort {
 	   runtimes for pivot at lastmember of the arr 
 	*/
        
-
-	System.out.println ("Runtimes for pivot point at last member of array");
-
-	System.out.println ("\nData set in powers of ten:\n");
-	for (int i = 1; i <= 10000000; i*=10)
-	    System.out.println("Duration for n = " + i + ": " + testSort_avg(i,3));
-
-
-	for (int i = 1; i <= 50; i++) {
-	    System.out.println ("Duration for n= " + i * 100000 + " : " + testSort_avg(10000 * i, 3));
+	System.out.println ("Created by Alitquan Mallick");
+	System.out.println ("\nRuntimes for pivot point at last member of array");
+	System.out.println ("Data set in powers of ten:\n");
+	System.out.println ("Data in formation (ARR LENGTH, RUNTIME)");
+	for (int i = 1; i <= 10000000; i*=10){
+	    System.out.println ("(" + i + ", " + testSort_avg(i,3) +" )");
+	    if (i > 100000) {
+		while (i < 10000000) {
+		     System.out.println ("(" + i + ", " + testSort_avg(i,3) +" )");
+		     i*=2;
+		}
+	    }
 	}
-
-	for (int i = 1; i <= 15; i++) {
-	    System.out.println ("Duration for n= " + i * 1000000 + " : " + testSort_avg(100000 * i, 3));
+	System.out.println("\n");
+	System.out.println ("Data in formation (ARR LENGTH, RUNTIME)");
+	System.out.println ("Created by Alitquan Mallick");
+	for (int i = 1; i <= 30; i++) {
+	    System.out.println ("(" + i * 10000 + ", " + testSort_avg(i * 10000,3) +" )");
 	}
+	System.out.println("\n");
+	System.out.println ("Data in formation (ARR LENGTH, RUNTIME)");
+	System.out.println ("Created by Alitquan Mallick");
+	for (int i = 1; i <= 10; i++) {
+	    System.out.println ("(" + i * 1000000 + ", " + testSort_avg(1000000 * i,3) +" )");
+	}
+	System.out.println("\n");
 
-	System.exit(0); // line that helps during testing; stops program at this point
+	//System.exit(0); // line that helps during testing; stops program at this point
 
 	System.out.println ("\nRuntimes for pivot point at midpoint  of array");
 
-	System.out.println ("\nData set in powers of ten:\n");
-	for (int i = 1; i <= 10000000; i*=10)
-	    System.out.println("Duration for n = " + i + ": " + testSort_avg(i,2));
-
-
-	for (int i = 1; i <= 50; i++) {
-	    System.out.println ("Duration for n= " + i * 100000 + " : " + testSort_avg(10000 * i, 2));
+	System.out.println ("Data set in powers of ten:\n");
+	System.out.println ("Data in formation (ARR LENGTH, RUNTIME)");
+	for (int i = 1; i <= 10000000; i*=10){
+	    System.out.println ("(" + i + ", " + testSort_avg(i,3) +" )");
+	    if (i > 100000) {
+		while (i < 10000000) {
+		     System.out.println ("(" + i + ", " + testSort_avg(i,2) +" )");
+		     i*=2;
+		}
+	    }
 	}
-
-	for (int i = 1; i <= 15; i++) {
-	    System.out.println ("Duration for n= " + i * 1000000 + " : " + testSort_avg(100000 * i, 2));
+	System.out.println("\n");
+	System.out.println ("Data in formation (ARR LENGTH, RUNTIME)");
+	System.out.println ("Created by Alitquan Mallick");
+	for (int i = 1; i <= 30; i++) {
+	    System.out.println ("(" + i * 10000 + ", " + testSort_avg(i * 100000,2) +" )");
 	}
-
+	System.out.println("\n");
+	System.out.println ("Data in formation (ARR LENGTH, RUNTIME)");
+	System.out.println ("Created by Alitquan Mallick");
+	for (int i = 1; i <= 10; i++) {
+	    System.out.println ("(" + i * 1000000 + ", " + testSort_avg(1000000 * i,2) +" )");
+	}
+	System.out.println("\n");
+	
+	System.out.println("\n");
+	System.out.println ("Created by Alitquan Mallick");
 	System.out.println ("\nRuntimes for pivot point at first member of array");
 
-	System.out.println ("\nData set in powers of ten:\n");
-	for (int i = 1; i <= 10000000; i*=10)
-	    System.out.println("Duration for n = " + i + ": " + testSort_avg(i,1));
-
-
-	for (int i = 1; i <= 50; i++) {
-	    System.out.println ("Duration for n= " + i * 100000 + " : " + testSort_avg(10000 * i, 1));
+	System.out.println ("Data set in powers of ten:\n");
+	System.out.println ("Data in formation (ARR LENGTH, RUNTIME)");
+	for (int i = 1; i <= 10000000; i*=10){
+	    System.out.println ("(" + i + ", " + testSort_avg(i,3) +" )");
+	    if (i > 100000) {
+		while (i < 10000000) {
+		     System.out.println ("(" + i + ", " + testSort_avg(i,1) +" )");
+		     i*=2;
+		}
+	    }
 	}
-
-	for (int i = 1; i <= 15; i++) {
-	    System.out.println ("Duration for n= " + i * 1000000 + " : " + testSort_avg(100000 * i, 1));
+	System.out.println("\n");
+	System.out.println ("Data in formation (ARR LENGTH, RUNTIME)");
+	System.out.println ("Created by Alitquan Mallick");
+	for (int i = 1; i <= 30; i++) {
+	    System.out.println ("(" + i * 10000 + ", " + testSort_avg(i * 10000,1) +" )");
 	}
+	System.out.println("\n");
+	System.out.println ("Data in formation (ARR LENGTH, RUNTIME)");
+	System.out.println ("Created by Alitquan Mallick");
+	for (int i = 1; i <= 10; i++) {
+	    System.out.println ("(" + i * 1000000 + ", " + testSort_avg(1000000 * i,1) +" )");
+	}
+	System.out.println("\n");
 	/*
 	System.out.println ("\nData set for interval of 20 from between 10 through 300:\n");
 	for (int x = 10; x <= 300; x+= 20)
